ae4b8650aa230e642bcdb0e5d03f4a69a4cace7b
Narendra Chaudhari <narendracv@gmail.com>
2021-12-25 18:09:44 +0530

    Add project files.

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,56 @@
+﻿using IotAdminAPI.Authentication;
+using IotAdminAPI.Models;
+using IotAdminAPI.Services;
+using IotAdminAPI.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IotAdminAPI.Controllers
+{
+    [ApiController]
+    [Route("API/[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+
+        private readonly ILogger<AuthenticationController> _logger;
+        private readonly IAuthenticationRepository authenticationRepository;
+        private readonly IConfiguration configuration;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger,
+            IAuthenticationRepository repository,
+            IConfiguration configuration)
+        {
+            _logger = logger;
+            authenticationRepository = repository;
+            this.configuration = configuration;
+        }
+
+
+
+        [HttpPost]
+        [Route("Token")]
+        public async Task<IActionResult> Token(Credentials credentials)
+        {
+            var user = await authenticationRepository.ValidateCredentials(credentials.UserName,
+                credentials.Password);
+
+
+            if (user == null) return NotFound();
+
+            string tokenSingingKey = this.configuration["TokenSigningKey"];
+
+            AuthenticationResponse token = JwtAuthentication.GenerateToken(user, tokenSingingKey);
+
+            return Ok(token);
+
+        }
+
+
+
+    }
+}
---
+++ ViewModel/AuthenticationResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace IotAdminAPI.ViewModel
+{
+    public class AuthenticationResponse
+    {
+        public AuthenticationResponse(string access_token, string token_type, double expires_in)
+        {
+            this.access_token = access_token;
+            this.token_type = token_type;
+            this.expires_in = expires_in;
+        }
+
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+
+        public double expires_in { get; set; }
+    }
+}
